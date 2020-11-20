fded32c83b3fb9f3e7b95ae8e2066163d9539618
Nvidia Jetson TX2 <nvidia.capstoneg01@gmail.com>
2020-11-19 21:31:25 -0600

    Removed sudo from jetson_inference script

--- dependencies/install_jetson_inference.sh
+++ dependencies/install_jetson_inference.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install git cmake
+apt-get install git cmake
 git clone --recursive https://github.com/dusty-nv/jetson-inference
 cd jetson-inference
 mkdir build
 cd build
 cmake ../
 make -j$(nproc)
-sudo make install
-sudo ldconfig
+make install
+ldconfig
 
 
 
